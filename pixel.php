e4201857ac71140e858981ab5730223831298e19
RAWLINGS <you@example.com>
2018-04-20 18:08:00 +0100

    hng task one

---
+++ pixel.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rawling Pixel</title>
+	<link rel="stylesheet" type="text/css" href="./css/pixel.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+</head>
+<body>
+	 <div class ="container">
+ 		<div id ="content">
+		<h1>RAWLINGS PIXELS</h1>
+		<h3>Redefining Photography and Graphics</h3>
+		<p><?php date_default_timezone_set("Africa/Lagos");
+			echo "The time is " . date("h:i:sa");?>
+			
+		</p>
+	</div>
+	</div>
+</body>
+</html>
